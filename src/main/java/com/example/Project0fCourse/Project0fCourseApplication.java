ad9478da5c46c2cccf0e9fc67d8339fdcc4abbd7
Hoàng Văn Chuẩn <hoangchuanks@gmail.com>
2025-05-16 21:34:55 +0700

    first commit

---
+++ src/main/java/com/example/Project0fCourse/Project0fCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project0fCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0fCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project0fCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Project0fCourse/Project0fCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project0fCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project0fCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fcfc9c3efb53023a33f6a6f9a2ca8376621e9c74
C <chinh99562@donga.edu.vn>
2025-05-17 13:18:26 +0700

    display members name

---
+++ src/main/java/com/example/Project0fCourse/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.Project0fCourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class MainController {
+    @GetMapping("/")
+    public String helloNah() {
+        return "index";
+    }
+}

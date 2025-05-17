c108808fbbdfbf2695ecf876f3560d53e51fe3a8
QUOC\len71 <len71231@gmail.com>
2025-05-17 13:46:16 +0700

    remove unused file

--- src/main/java/com/example/Project0fCourse/controller/MainController.java
+++
@@ -1,9 +0,0 @@
-package com.example.Project0fCourse.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-class MainController {
-
-}
